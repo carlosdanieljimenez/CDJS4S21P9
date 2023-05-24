134e0217ee2d5b12911f83b587c9c34132ee207b
carlosdanieljimenez <carlosdaniel20201000@gmail.com>
2023-05-23 23:18:08 -0600

    carga inicial

---
+++ src/CDJS4S21P9/CDJS4S21P9.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CDJS4S21P9;
+
+/**
+ *
+ * @author tesoem
+ */
+public class CDJS4S21P9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new JFPrincipal().setVisible(true);
+    }
+    
+}
